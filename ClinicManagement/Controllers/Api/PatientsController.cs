269e61c580a23cc66903ac12ea18fdf1d33c0c4d
parmar441 <bhavinparmar441@gmail.com>
2021-12-04 13:09:53 -0500

    Committed after adding API

--- ClinicManagement/Controllers/Api/PatientsController.cs
+++ ClinicManagement/Controllers/Api/PatientsController.cs
@@ -36,7 +36,32 @@ namespace ClinicManagement.Controllers.Api
             _unitOfWork.Patients.Remove(patient);
             _unitOfWork.Complete();
             return Ok();
+        }      
+
+        [Route("~/api/Delete/")]
+        [HttpDelete]
+        public IHttpActionResult DeleteData([FromBody] int id)
+        {
+            var patient = _unitOfWork.Patients.GetPatient(id);
+            _unitOfWork.Patients.Remove(patient);
+            _unitOfWork.Complete();
+            return Ok();
         }
 
+        [Route("~/api/GetAppointments/")]
+        [HttpPost]
+        public Appointment Details(int id)
+        {
+            var appointment = _unitOfWork.Appointments.GetAppointmentWithPatient(id);
+            return Mapper.Map<Appointment>(appointment);
+        }
+
+        [Route("~/api/GetAllAppointments/")]
+        [HttpGet]
+        public Appointment Index()
+        {
+            var appointments = _unitOfWork.Appointments.GetAppointments();
+            return Mapper.Map<Appointment>(appointments);
+        }
     }
 }
